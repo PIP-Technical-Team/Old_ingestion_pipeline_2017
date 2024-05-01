0f81aa0e62a07db7d5b26532858900cee9eed0f2
R.Andres Castaneda <acastastanedaa@worldbank.org>
2024-04-30 21:09:45 -0400

    update group to synthetic

--- _sandbox/gd_2_synth.R
+++ _sandbox/gd_2_synth.R
@@ -6,6 +6,7 @@ withr::local_options(list(joyn.verbose = FALSE))
 py <- 2017
 
 pipload::pip_load_all_aux(replace = TRUE)
+gls <- pipfun::pip_create_globals()
 
 
 setnames(gdm, 
@@ -54,12 +55,12 @@ gpfw <- joyn::joyn(pfw[use_groupdata == 1], gdm,
 
 # filter data -----------
 
-cts <- NULL
-yrs <- NULL
 
 cts <- "CHN"
 yrs <- 1981
 
+cts <- NULL
+yrs <- NULL
 
 if (!is.null(cts)) {
   gpfw <- gpfw[country_code %in% cts]
@@ -75,48 +76,85 @@ year    <- gpfw[, year]
 
 
 # Process in functional programming -----------
-x       <- country
-y       <- year
-
-## local cache data ------------
-dt <- pipload::pip_load_data(x,y)
-
-area_levels <- dt[, unique(area)]
-lsyn        <- vector("list", length(area_levels))
-names(lsyn) <- area_levels
-
-# i <- area_levels[1]
-for (i in area_levels) {
+j <- 153
+ldt <- purrr::map(cli::cli_progress_along(1:nrow(gpfw)), \(j) {
+  gpfw_j <- gpfw[j]
   
-  ## Filter data -------------
-  dt_area   <- dt[area == i]
-  gpfw_area <- gpfw[data_level  == i]
-  gd_type   <- sub("\\D", "", gpfw_area[, gd_type]) |> 
-    as.numeric()
+  ## local cache data ------------
+  dt <- pipload::pip_load_data(gpfw_j$country,
+                               gpfw_j$year, 
+                               verbose = FALSE)
   
-  ## clean group data ------------
   
-  dt_area <- wbpip:::gd_clean_data(dt_area, 
-                                   welfare     = "welfare",
-                                   population  = "weight", 
-                                   gd_type     = gd_type)
+  area_levels <- dt[, unique(area)]
+  lal         <- length(area_levels)
+  if (lal == 1) {
+    if (area_levels == "" || is.na(area_levels) || is.null(area_levels)) {
+      area_levels <- "national"
+      dt[, area := "national"]
+    }
+  }
+  lsyn        <- vector("list", length = lal)
+  names(lsyn) <- area_levels
   
-  ## Create synthetic data ------------
-  ls <- wbpip:::sd_create_synth_vector(welfare     = dt_area[, welfare], 
-                                       population  = dt_area[, weight], 
-                                       mean  = gpfw_area[, survey_mean_lcu],
-                                       pop   = gpfw_area[, pop])
-  ls[, `:=`(
-    welfare_lcu = welfare,
-    area = i
-  )][, 
-     welfare_ppp := wbpip:::deflate_welfare_mean(welfare_mean = welfare, 
-                                                 ppp = gpfw_area[, ppp], 
-                                                 cpi = gpfw_area[, cpi])
-  ]
   
-  lsyn[[i]] <- ls
-}
-
-## Combine data ------------
-dt <- rbindlist(lsyn)
+  # i <- area_levels[2]
+  for (i in area_levels) {
+    
+    ## Filter data -------------
+    dt_area   <- dt[area == i]
+    gd_type   <- sub("\\D", "", gpfw_j$gd_type) |> 
+      as.numeric() 
+    
+    ## clean group data ------------
+    
+    dt_area <- wbpip:::gd_clean_data(dt_area, 
+                                     welfare     = "welfare",
+                                     population  = "weight", 
+                                     gd_type     = gd_type, 
+                                     quiet       = TRUE)
+    
+    ## Create synthetic data ------------
+    # empty data.table when error or warning
+    emp <- data.table(country_code = gpfw_j$country,
+                      year = gpfw_j$year, 
+                      are = i) |> 
+      _[, c("welfare", "welfare_lcu", "welfare_ppp") := NA]
+    
+    # computation
+    ls <- tryCatch(
+      expr = {
+        # Your code...
+        wbpip:::sd_create_synth_vector(welfare     = dt_area$welfare, 
+                                       population  = dt_area$weight, 
+                                       mean        = gpfw_j$survey_mean_lcu,
+                                       pop         = gpfw_j$pop)
+        ls[, `:=`(
+          welfare_lcu = welfare,
+          area = i,
+          country_code = gpfw_j$country,
+          year = gpfw_j$year
+        )][, 
+           welfare_ppp := wbpip:::deflate_welfare_mean(welfare_mean = welfare, 
+                                                       ppp = gpfw_j$ppp, 
+                                                       cpi = gpfw_j$cpi)
+        ]
+      }, # end of expr section
+      
+      error = function(e) {
+        emp
+      }, # end of error section
+      
+      warning = function(w) {
+        emp
+      }
+    ) # End of trycatch
+    
+    lsyn[[i]] <- ls
+  }
+  
+  ## Combine data ------------
+  dt <- rbindlist(lsyn)
+  
+  return(dt)
+})
